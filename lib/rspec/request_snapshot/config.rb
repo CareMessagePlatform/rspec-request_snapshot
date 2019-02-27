ef527c872d07c97aab15d01ff0494b412b44090c
travis <travis.gaff@verbasoftware.com>
2019-02-27 15:09:33 -0800

    allow configuring a default format other than json

--- lib/rspec/request_snapshot/config.rb
+++ lib/rspec/request_snapshot/config.rb
@@ -3,5 +3,6 @@ module Rspec::RequestSnapshot
     config.add_setting :request_snapshots_dir, default: "spec/fixtures/snapshots"
     config.add_setting :request_snapshots_dynamic_attributes, default: %w(id created_at updated_at)
     config.add_setting :request_snapshots_ignore_order, default: %w()
+    config.add_setting :request_snapshots_default_format, default: :json
   end
 end
